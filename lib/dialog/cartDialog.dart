103553c998d9e61b8d908878e5f95f2c83be89b4
bagassj <bagasseptian9762@gmail.com>
2022-04-25 16:41:56 +0700

    fix CartDialog

--- lib/dialog/cartDialog.dart
+++ lib/dialog/cartDialog.dart
@@ -11,167 +11,193 @@ class _cartDialogState extends State<cartDialog> {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              // color: mGray,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
               children: [
-                Text(
-                  'Your Cart',
-                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
-                ),
-                Text('total: 1.500.000',
-                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 14))
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              margin: EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(5)),
-              child: Row(
-                children: [
-                  Container(
-                    height: 48,
-                    width: 48,
-                    margin: EdgeInsets.only(left: 10),
-                    decoration: BoxDecoration(
-                        color: mGray, borderRadius: BorderRadius.circular(4)),
-                    child: Image.asset('assets/Semen.png'),
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Your Cart',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700, fontSize: 18),
+                      ),
+                      Text('total: 1.500.000',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w700, fontSize: 14))
+                    ],
                   ),
-                  Container(
-                    height: 48,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                    flex: 8,
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.max,
-                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Container(
-                          margin: EdgeInsets.only(left: 5),
-                          child: Text(
-                            'Semen Tiga Roda',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
-                            ),
+                          width: MediaQuery.of(context).size.width,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5),
                           ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.remove_circle_outline,
-                              size: 24,
-                            ),
-                            Container(
-                              margin: EdgeInsets.only(left: 5, right: 5),
-                              child: Text(
-                                '10',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: 10,
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 48,
+                                width: 48,
+                                margin: EdgeInsets.only(left: 10),
+                                decoration: BoxDecoration(
+                                    color: mGray,
+                                    borderRadius: BorderRadius.circular(4)),
+                                child: Image.asset('assets/Semen.png'),
+                              ),
+                              Container(
+                                height: 48,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(left: 5),
+                                      child: Text(
+                                        'Semen Tiga Roda',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 12,
+                                        ),
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.remove_circle_outline,
+                                          size: 24,
+                                        ),
+                                        Container(
+                                          margin: EdgeInsets.only(
+                                              left: 5, right: 5),
+                                          child: Text(
+                                            '10',
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              fontSize: 10,
+                                            ),
+                                          ),
+                                        ),
+                                        Icon(
+                                          Icons.control_point,
+                                          size: 24,
+                                        ),
+                                      ],
+                                    )
+                                  ],
                                 ),
                               ),
-                            ),
-                            Icon(
-                              Icons.control_point,
-                              size: 24,
-                            ),
-                          ],
-                        )
+                              GestureDetector(
+                                onTap: (() {}),
+                                child: Container(
+                                  height: 48,
+                                  margin: EdgeInsets.only(left: 10),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Container(
+                                        child: Text(
+                                          '1.500.000',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 10,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        alignment: Alignment.center,
+                                        padding: EdgeInsets.only(
+                                          top: 5,
+                                          bottom: 5,
+                                          left: 10,
+                                          right: 10,
+                                        ),
+                                        decoration: BoxDecoration(
+                                            color: mRed,
+                                            borderRadius:
+                                                BorderRadius.circular(5)),
+                                        child: Text(
+                                          'Delete -',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                            fontSize: 10,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: (() {}),
+                    )),
+                Expanded(
+                  flex: 1,
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => progress()));
+                    },
                     child: Container(
-                      height: 48,
-                      margin: EdgeInsets.only(left: 10),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Container(
-                            child: Text(
-                              '1.500.000',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            alignment: Alignment.center,
-                            padding: EdgeInsets.only(
-                              top: 5,
-                              bottom: 5,
-                              left: 10,
-                              right: 10,
-                            ),
-                            decoration: BoxDecoration(
-                                color: mRed,
-                                borderRadius: BorderRadius.circular(5)),
-                            child: Text(
-                              'Delete -',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                                fontSize: 10,
-                              ),
-                            ),
-                          ),
-                        ],
+                      width: MediaQuery.of(context).size.width * 0.2,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.only(
+                        top: 5,
+                        bottom: 5,
+                        left: 10,
+                        right: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        color: mGreen,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text(
+                        'BUY',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 10,
+                        ),
                       ),
                     ),
                   ),
-                ],
-              ),
+                )
+              ],
             ),
-            SizedBox(
-              height: 10,
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => progress()));
-              },
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.2,
-                alignment: Alignment.center,
-                padding: EdgeInsets.only(
-                  top: 5,
-                  bottom: 5,
-                  left: 10,
-                  right: 10,
-                ),
-                decoration: BoxDecoration(
-                  color: mGreen,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Text(
-                  'BUY',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
